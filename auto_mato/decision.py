04e222937615481b5c1ddd714d9114abb312b0cd
Doruk Tayli <doruktayli@gmail.com>
2021-07-20 16:32:35 -0700

    black and isort formatting

--- auto_mato/decision.py
+++ auto_mato/decision.py
@@ -7,13 +7,10 @@ Created on Mon Jul 19 08:35:24 2021
 
 import pandas as pd
 
-
-df = pd.read_csv('input.csv')
-input_data =  df.columns[0]
+df = pd.read_csv("input.csv")
+input_data = df.columns[0]
 
 data = float(input_data)
 
 if data < 0.15:
-    print(' RMSE is good enough, continue')
-
-
+    print(" RMSE is good enough, continue")
--- auto_mato/prediction.py
+++ auto_mato/prediction.py
@@ -11,70 +11,69 @@ Modified on Tue Aug 25 18:15:10 2020
 
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error 
-from sklearn.metrics import mean_squared_error
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from tensorflow import keras
-
 import csv
 
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
 # Neural network
-model = Sequential([
-  Dense(16,  input_dim=49, kernel_initializer='normal', activation='relu'),
-  Dense(16, activation='relu'),
-  Dense(1, activation='sigmoid'),
-])
+model = Sequential(
+    [
+        Dense(16, input_dim=49, kernel_initializer="normal", activation="relu"),
+        Dense(16, activation="relu"),
+        Dense(1, activation="sigmoid"),
+    ]
+)
 
 
-df = pd.read_csv('dataset1.csv')
-df = df[df.columns.difference(['Unnamed: 0'])]
+df = pd.read_csv("dataset1.csv")
+df = df[df.columns.difference(["Unnamed: 0"])]
 
 
+input_data = df.iloc[:, :49].values
+label_MOS = df["MOS"].values
 
-input_data =  df.iloc[:,:49].values
-label_MOS = df['MOS'].values
 
+train_X, val_X, train_y, val_y = train_test_split(
+    input_data, label_MOS, test_size=0.25, random_state=14
+)
 
-train_X, val_X, train_y, val_y = train_test_split(input_data, label_MOS, test_size = 0.25, random_state = 14)
+x_train = train_X
+y_train = train_y
 
-x_train = train_X 
-y_train =  train_y
-
-x_test =  val_X
-y_test =  val_y
+x_test = val_X
+y_test = val_y
 
 
 # COMPILE
-keras.optimizers.Adam(
-   learning_rate = 0.01, beta_1 = 0.9, beta_2 = 0.999, amsgrad = False
-)
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
+keras.optimizers.Adam(learning_rate=0.01, beta_1=0.9, beta_2=0.999, amsgrad=False)
+model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mae"])
 
 
-#TRAINING
+# TRAINING
 history = model.fit(x_train, y_train, epochs=200, batch_size=20)
 
-    
-NNpredictions = model.predict(x_test) 
+
+NNpredictions = model.predict(x_test)
 
 
-MAE = mean_absolute_error(val_y , NNpredictions)
+MAE = mean_absolute_error(val_y, NNpredictions)
 
-print('Neural Network validation MAE = ',MAE)
-RMSE = mean_squared_error(val_y , NNpredictions, squared = False)
+print("Neural Network validation MAE = ", MAE)
+RMSE = mean_squared_error(val_y, NNpredictions, squared=False)
 
-print('Neural Network validation RMSE = ',RMSE)
+print("Neural Network validation RMSE = ", RMSE)
 
 ip = [RMSE]
 
-#Write RMSE as input to other xApp
-with open('input.csv', 'w') as f:
+# Write RMSE as input to other xApp
+with open("input.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerow(ip)
 
 # save ML model
-model.save('TrainedMLmodel.h5')
+model.save("TrainedMLmodel.h5")
